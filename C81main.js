9b80a3b754c28fc2e024258ea43c97a0827e7126
HmanIsCool13 <116886132+HmanIsCool13@users.noreply.github.com>
2023-01-09 17:46:48 -0500

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,36 @@
+
+canvas =
+document.getElementById("myCanvas")
+ctx = canvas.getcontext("2d")
+ctx.beginpath();
+ctx.lineWidth =1;
+ctx.stroke();
+ctx.rect(150,143,430,200)
+ctx.strokeStyle = Red
+
+ctx.beginpath();
+ctx.lineWidth =1;
+ctx.stroke();
+ctx.rect(150,143,430,200)
+ctx.strokeStyle =  Blue
+
+ctx = canvas.getcontext("2d")
+ctx.beginpath();
+ctx.lineWidth =1;
+ctx.stroke();
+ctx.rect(150,143,430,200)
+ctx.strokeStyle = black
+
+ctx = canvas.getcontext("2d")
+ctx.beginpath();
+ctx.lineWidth =1;
+ctx.stroke();
+ctx.rect(150,143,430,200)
+ctx.strokeStyle = green
+
+ctx = canvas.getcontext("2d")
+ctx.beginpath();
+ctx.lineWidth =1;
+ctx.stroke();
+ctx.rect(150,143,430,200)
+ctx.strokeStyle = yellow
